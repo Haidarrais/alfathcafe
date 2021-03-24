d1851c4214909af28ef5d082207eae31e783548f
Haidar Rais <abdillah.fams@gmail.com>
2021-03-24 17:50:32 +0700

    items

--- components/Content.vue
+++ components/Content.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="grid grid-cols-4 gap-4 p-24">
-    <div class="p-3 border-2 shadow rounded-md  text-center">
+    <div class="p-5 border-2 shadow rounded-md  text-center">
       <img src="~/static/food.png" alt="">
       <h3 class="font-bold text-center">Indomie Spesial</h3>
-      <p>IDR 8K</p>
-      <button class="bg-black hover:bg-gray-700 rounded-full h-8 w-8 font-black -mb-8 text-white">+</button>
+      <p>IDR 12K</p>
+      <button class="bg-black hover:bg-gray-700 rounded-full h-12 w-12 font-black -ml-6 absolute text-white">+</button>
     </div>
-    <div class="p-3 border-2 shadow rounded-md  text-center">
+    <div class="p-5 border-2 shadow rounded-md  text-center">
       <img src="~/static/food1.png" alt="">
       <h3 class="font-bold">Biryani Spesial</h3>
-      <p>IDR 8K</p>
-      <button class="bg-black hover:bg-gray-700 rounded-full h-8 w-8 font-black -mb-8 text-white">+</button>
+      <p>IDR 18K</p>
+      <button class="bg-black hover:bg-gray-700 rounded-full h-12 w-12 font-black -ml-6 absolute text-white">+</button>
     </div>
-    <div class="p-3 border-2 shadow rounded-md  text-center">
-      <img src="~/static/food3.png" alt="">
+    <div class="p-5 border-2 shadow rounded-md text-center">
+      <img src="~/static/food2.png" alt="">
       <h3 class="font-bold">Salad Spesial</h3>
-      <p>IDR 8K</p>
-      <button class="bg-black hover:bg-gray-700 rounded-full h-8 w-8 font-black -mb-8 text-white">+</button>
+      <p>IDR 15K</p>
+      <button class="bg-black hover:bg-gray-700 rounded-full h-12 w-12 font-black -ml-6 absolute text-white">+</button>
     </div>
   </div>
 </template>
